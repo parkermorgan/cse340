1a3e7e6d87ff6ba51b2be4b9e955fc0c0885f12d
parkermorgan <156543039+parkermorgan@users.noreply.github.com>
2025-02-01 19:14:53 -0700

    completed week 4 prepares

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/index")
 
 /* ***********************
  * View engine and Templates
@@ -21,7 +22,7 @@ app.use(expressLayouts)
 app.set("layout", "./layouts/layout")
 
 //Index route
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
@@ -30,6 +31,27 @@ app.use("/inv", inventoryRoute)
  * Routes
  *************************/
 app.use(static)
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
 
 /* ***********************
  * Local Server Information
